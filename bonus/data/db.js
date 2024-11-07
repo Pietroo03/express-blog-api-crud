9ab567db7bbe16abbc3abf9ffcfa9f6e3acd4fd4
Pietroo03 <pietro.ponte.03@gmail.com>
2024-11-07 16:37:58 +0100

    add data folder

---
+++ bonus/data/db.js
@@ -0,0 +1,35 @@
+const pokemon = [
+    {
+      name: "Pikachu",
+      type: "Electric",
+      level: 25
+    },
+    {
+      name: "Charmander",
+      type: "Fire",
+      level: 15
+    },
+    {
+      name: "Bulbasaur",
+      type: "Grass/Poison",
+      level: 12
+    },
+    {
+      name: "Squirtle",
+      type: "Water",
+      level: 10
+    },
+    {
+      name: "Eevee",
+      type: "Normal",
+      level: 20
+    },
+    {
+      name: "Jigglypuff",
+      type: "Fairy/Normal",
+      level: 18
+    }
+  ];
+  
+module.exports = pokemon
+  
