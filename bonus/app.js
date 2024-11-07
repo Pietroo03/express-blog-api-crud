bf9e736c359785fc4bf80df8c38e62f4a40af39f
Pietroo03 <pietro.ponte.03@gmail.com>
2024-11-07 16:58:33 +0100

    add show method

--- bonus/app.js
+++ bonus/app.js
@@ -5,21 +5,10 @@ const pokemonController = require('./controllers/pokemonController.js')
 
 app.listen(3001, () => {
     console.log('Server started on port 3001');
-    
 })
 
 app.get('/pokemons', pokemonController.index)
 
-app.get('/pokemons/:name', (req, res) => {
-    const pokemon = pokemons.find((pokemon) => pokemon.name.toLowerCase() === req.params.name)
+app.get('/pokemons/:name', pokemonController.show)
 
-    if (!pokemon) {
-        return res.status(404).json({
-            error: `Nessun pokemon trovato con il nome di ${req.params.name} `
-        })
-    }
 
-    return res.status(200).json({
-        data: pokemon
-    })
-})
--- bonus/controllers/pokemonController.js
+++ bonus/controllers/pokemonController.js
@@ -7,6 +7,21 @@ const index = (req, res) => {
     })
 }
 
+const show = (req, res) => {
+    const pokemon = pokemons.find((pokemon) => pokemon.name.toLowerCase() === req.params.name)
+
+    if (!pokemon) {
+        return res.status(404).json({
+            error: `Nessun pokemon trovato con il nome di ${req.params.name} `
+        })
+    }
+
+    return res.status(200).json({
+        data: pokemon
+    })
+}
+
 module.exports = {
-    index
+    index,
+    show
 }
