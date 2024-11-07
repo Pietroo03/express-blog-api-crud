ccff6009596fe452eab964270a2aec9cc8466456
Pietroo03 <pietro.ponte.03@gmail.com>
2024-11-07 18:00:58 +0100

    add middlewarer

--- bonus/app.js
+++ bonus/app.js
@@ -2,11 +2,14 @@ const express = require('express')
 const app = express()
 const routes = require('./routes/pokemon.js')
 const notFoundMiddleware = require('./middlewares/notFound.js')
+const loggerMiddleware = require('./middlewares/loggerMiddleware.js')
 app.use(express.json())
 
 app.listen(3001, () => {
     console.log('Server started on port 3001');
 })
+
+app.use('/pokemons', loggerMiddleware)
 
 app.use('/pokemons', routes)
 
