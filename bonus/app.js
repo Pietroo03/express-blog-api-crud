9a108540c2829519468d83dd1bd1c7251e700b4a
Pietroo03 <pietro.ponte.03@gmail.com>
2024-11-07 16:42:42 +0100

    add endpoint get method

--- bonus/app.js
+++ bonus/app.js
@@ -2,7 +2,14 @@ const express = require('express')
 const app = express()
 const pokemon = require('./data/db.js')
 
-app.listen(3000, () => {
-    console.log('Server started on port 3000');
+app.listen(3001, () => {
+    console.log('Server started on port 3001');
     
+})
+
+app.get('/pokemon', (req, res) => {
+    res.json({
+        data: pokemon,
+        counter: pokemon.length
+    })
 })
