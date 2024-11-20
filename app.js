a4e7220fb119aa58cdbb4cab2a1e85cfe4a7c806
Pietroo03 <pietro.ponte.03@gmail.com>
2024-11-20 15:16:26 +0100

    add cors

--- app.js
+++ app.js
@@ -1,10 +1,17 @@
 const express = require('express')
+const cors = require('cors')
 const PostsRouter = require('./routers/posts.js')
-const notFoundMiddleware = require('./middlewares/notFound.js')
 const loggerMiddleware = require('./middlewares/loggerMiddleware.js')
 const app = express()
 const host = 'http://127.0.0.1'
 const port = 3000
+
+const corsOptions = {
+    credentials: true,
+    origin: ['http://localhost:3000', 'http://localhost:5173']
+};
+
+app.use(cors(corsOptions));
 
 app.use(express.json())
 app.use(express.static('public'))
@@ -13,17 +20,11 @@ app.listen(port, () => {
     console.log(`Example app listening on ${host}:${port}`)
 })
 
-app.use('/posts', (req, res, next) => {
-    throw new Error('You broke everything!')
-})
-
 app.use('/posts', loggerMiddleware)
 
 app.use('/posts', PostsRouter)
 
-app.use(notFoundMiddleware)
-
-app.use ((err, req, res, next) => {
+app.use((err, req, res, next) => {
     console.log('Error: ', err.message)
 
     console.error(err.stack)
